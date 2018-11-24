7118862bd3ec87a209acc5195f017151bcb4c613
csvk <chanda.souvik@gmail.com>
2018-11-24 13:03:51 +0530

    Added template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello Peturam!'
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
